cf9fc47a5cc17f1baccb65ca9a651b07c019b315
Karollainny Teles <karollainnyteles@hotmail.com>
2022-02-09 00:13:22 -0300

    Configurando DbContext

---
+++ src/Coodesh.Challenge.Data/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Coodesh.Challenge.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coodesh.Challenge.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Launch> Launches { get; set; }
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
