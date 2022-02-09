a63894a1279bf24806aeebc7bfb3d65ab36b3277
Karollainny Teles <karollainnyteles@hotmail.com>
2022-02-09 20:54:18 -0300

    Implementando repositórios

---
+++ src/Coodesh.Challenge.Data/Repositories/ArticleReadOnlyRepository.cs
@@ -0,0 +1,33 @@
+﻿using Coodesh.Challenge.Business.Contracts.Repositories;
+using Coodesh.Challenge.Business.Models;
+using Coodesh.Challenge.Business.Parameters;
+using Coodesh.Challenge.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coodesh.Challenge.Data.Repositories
+{
+    public class ArticleReadOnlyRepository : IArticleReadOnlyRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ArticleReadOnlyRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Article>> FindAsync(PaginationParameters paginationParameters)
+        {
+            return await _dbContext.Articles
+                .Skip(paginationParameters.Skip)
+                .Take(paginationParameters.PageSize).ToListAsync();
+        }
+
+        public async Task<Article> GetByIdAsync(int id)
+        {
+            return await _dbContext.Articles.FindAsync(id);
+        }
+    }
+}
---
+++ src/Coodesh.Challenge.Data/Repositories/ArticleWriteOnlyRepository.cs
@@ -0,0 +1,35 @@
+﻿using Coodesh.Challenge.Business.Contracts.Repositories;
+using Coodesh.Challenge.Business.Models;
+using Coodesh.Challenge.Data.Context;
+using System.Threading.Tasks;
+
+namespace Coodesh.Challenge.Data.Repositories
+{
+    public class ArticleWriteOnlyRepository : IArticleWriteOnlyRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ArticleWriteOnlyRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddAsync(Article entity)
+        {
+            await _dbContext.Articles.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            _dbContext.Articles.Remove(new Article { Id = id });
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Article entity)
+        {
+            _dbContext.Articles.Update(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
