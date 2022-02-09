1bd21c4c1b471cc9fa678d120edd05bbe94b8e7d
Karollainny Teles <karollainnyteles@hotmail.com>
2022-02-08 23:18:32 -0300

    Adicionando descrição ao swagger

--- src/Coodesh.Challenge.Api/Startup.cs
+++ src/Coodesh.Challenge.Api/Startup.cs
@@ -1,16 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Coodesh.Challenge.Api
 {
@@ -29,7 +22,12 @@ namespace Coodesh.Challenge.Api
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Coodesh.Challenge.Api", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Coodesh.Challenge.Api",
+                    Description = "Api desenvolvida como proposta para challenge da Coodesh",
+                    Version = "v1"
+                });
             });
         }
 
