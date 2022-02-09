99ef60d931adafa6b5796c3dcd63318fcaee63a9
Karollainny Teles <karollainnyteles@hotmail.com>
2022-02-09 20:53:27 -0300

    Add PaginationParameters

---
+++ src/Coodesh.Challenge.Business/Parameters/PaginationParameters.cs
@@ -0,0 +1,9 @@
+﻿namespace Coodesh.Challenge.Business.Parameters
+{
+    public class PaginationParameters
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; } = 10;
+        public int Skip => PageSize * PageNumber;
+    }
+}
