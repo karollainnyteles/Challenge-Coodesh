ddb925e98017d348ad216ba11e22bd445bad42df
Karollainny Teles <karollainnyteles@hotmail.com>
2022-02-09 20:54:42 -0300

    add extensions

---
+++ src/Coodesh.Challenge.Api/Extensions/RepositoriesConfig.cs
@@ -0,0 +1,17 @@
+﻿using Coodesh.Challenge.Business.Contracts.Repositories;
+using Coodesh.Challenge.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Coodesh.Challenge.Api.Extensions
+{
+    public static class RepositoriesConfig
+    {
+        public static IServiceCollection AddRepositoriesConfig(this IServiceCollection services)
+        {
+            services.AddScoped<IArticleReadOnlyRepository, ArticleReadOnlyRepository>();
+            services.AddScoped<IArticleWriteOnlyRepository, ArticleWriteOnlyRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ src/Coodesh.Challenge.Api/Extensions/SwaggerConfig.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Coodesh.Challenge.Api.Extensions
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Coodesh.Challenge.Api",
+                    Description = "Api desenvolvida como proposta para challenge da Coodesh",
+                    Version = "v1"
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfig(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Coodesh.Challenge.Api v1"));
+
+            return app;
+        }
+    }
+}
