1d7c8a94fd6de382b3af806f8daa309fa3a29d9a
Karollainny Teles <karollainnyteles@hotmail.com>
2022-02-10 00:05:39 -0300

    removenmdo jsonIgnore

--- src/Coodesh.Challenge.Business/Models/Event.cs
+++ src/Coodesh.Challenge.Business/Models/Event.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Text.Json.Serialization;
 
 namespace Coodesh.Challenge.Business.Models
 {
     public class Event : Entity<Guid>
     {
-        [JsonIgnore]
         public int ArticleId { get; set; }
 
         public string Provider { get; set; }
 
-        [JsonIgnore]
         public Article Article { get; set; }
     }
 }
