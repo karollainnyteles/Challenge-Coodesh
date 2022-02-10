77ff0873a950b7620bb08fa88305faa41f25c351
Karollainny Teles <karollainnyteles@hotmail.com>
2022-02-09 22:39:03 -0300

    add Queries para Find

---
+++ src/Coodesh.Challenge.Query/Queries/Articles/Find/FindArticlesQuery.cs
@@ -0,0 +1,17 @@
+﻿using Coodesh.Challenge.Business.Models;
+using Coodesh.Challenge.Business.Parameters;
+using MediatR;
+using System.Collections.Generic;
+
+namespace Coodesh.Challenge.Query.Queries.Articles.Find
+{
+    public class FindArticlesQuery : IRequest<IEnumerable<Article>>
+    {
+        public PaginationParameters Parameters { get; set; }
+
+        public FindArticlesQuery(PaginationParameters parameters)
+        {
+            Parameters = parameters;
+        }
+    }
+}
---
+++ src/Coodesh.Challenge.Query/Queries/Articles/Find/FindArticlesQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Coodesh.Challenge.Business.Contracts.Repositories;
+using Coodesh.Challenge.Business.Models;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Coodesh.Challenge.Query.Queries.Articles.Find
+{
+    public class FindArticlesQueryHandler : IRequestHandler<FindArticlesQuery, IEnumerable<Article>>
+    {
+        private readonly IArticleReadOnlyRepository _articleReadOnlyRepository;
+
+        public FindArticlesQueryHandler(IArticleReadOnlyRepository articleReadOnlyRepository)
+        {
+            _articleReadOnlyRepository = articleReadOnlyRepository;
+        }
+
+        public async Task<IEnumerable<Article>> Handle(FindArticlesQuery request, CancellationToken cancellationToken)
+        {
+            var articles = await _articleReadOnlyRepository.FindAsync(request.Parameters);
+
+            return articles;
+        }
+    }
+}
