a45036c46704ef3aa0decdac3c83f8af80ea6490
Karollainny Teles <karollainnyteles@hotmail.com>
2022-02-09 22:40:06 -0300

    Add queries do GetById

---
+++ src/Coodesh.Challenge.Query/Queries/Articles/GetById/GetArticleByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using Coodesh.Challenge.Business.Models;
+using MediatR;
+
+namespace Coodesh.Challenge.Query.Queries.Articles.GetById
+{
+    public class GetArticleByIdQuery : IRequest<Article>
+    {
+        public int Id { get; set; }
+
+        public GetArticleByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ src/Coodesh.Challenge.Query/Queries/Articles/GetById/GetArticleByIdQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Coodesh.Challenge.Business.Contracts.Repositories;
+using Coodesh.Challenge.Business.Models;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Coodesh.Challenge.Query.Queries.Articles.GetById
+{
+    public class GetArticleByIdQueryHandler : IRequestHandler<GetArticleByIdQuery, Article>
+    {
+        private readonly IArticleReadOnlyRepository _articleReadOnlyRepository;
+
+        public GetArticleByIdQueryHandler(IArticleReadOnlyRepository articleReadOnlyRepository)
+        {
+            _articleReadOnlyRepository = articleReadOnlyRepository;
+        }
+
+        public async Task<Article> Handle(GetArticleByIdQuery request, CancellationToken cancellationToken)
+        {
+            var article = await _articleReadOnlyRepository.GetByIdAsync(request.Id);
+
+            return article;
+        }
+    }
+}
