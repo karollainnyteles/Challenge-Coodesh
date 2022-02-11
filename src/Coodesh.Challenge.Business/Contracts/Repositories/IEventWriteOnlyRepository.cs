0eb02a632532786f639d23bdc2d4eb80efb2edca
Karollainny Teles <karollainnyteles@hotmail.com>
2022-02-11 00:33:05 -0300

    add interfaces de repositórios write only de Event e Launch

---
+++ src/Coodesh.Challenge.Business/Contracts/Repositories/IEventWriteOnlyRepository.cs
@@ -0,0 +1,9 @@
+﻿using Coodesh.Challenge.Business.Models;
+using System;
+
+namespace Coodesh.Challenge.Business.Contracts.Repositories
+{
+    public interface IEventWriteOnlyRepository : IWriteOnlyRepository<Event, Guid>
+    {
+    }
+}
