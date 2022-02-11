1416bf7d97e0a70bdc4af48302910f7bfd9c471c
Karollainny Teles <karollainnyteles@hotmail.com>
2022-02-11 00:31:40 -0300

    implementando SaveChanges

--- src/Coodesh.Challenge.Data/Repositories/ArticleWriteOnlyRepository.cs
+++ src/Coodesh.Challenge.Data/Repositories/ArticleWriteOnlyRepository.cs
@@ -17,19 +17,21 @@ namespace Coodesh.Challenge.Data.Repositories
         public async Task AddAsync(Article entity)
         {
             await _dbContext.Articles.AddAsync(entity);
-            await _dbContext.SaveChangesAsync();
         }
 
         public async Task RemoveAsync(int id)
         {
             _dbContext.Articles.Remove(new Article { Id = id });
-            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _dbContext.SaveChangesAsync();
         }
 
         public async Task UpdateAsync(Article entity)
         {
             _dbContext.Articles.Update(entity);
-            await _dbContext.SaveChangesAsync();
         }
     }
 }
