ffa17bdad2a32dec5e9235f446c49197a59679b7
Karollainny Teles <karollainnyteles@hotmail.com>
2022-02-11 00:37:27 -0300

    ativando visualização de queries do EF

--- src/Coodesh.Challenge.Data/Context/AppDbContext.cs
+++ src/Coodesh.Challenge.Data/Context/AppDbContext.cs
@@ -1,12 +1,23 @@
 ﻿using Coodesh.Challenge.Business.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace Coodesh.Challenge.Data.Context
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        { }
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors()
+                .LogTo(Console.WriteLine, LogLevel.Information);
+        }
 
         public DbSet<Article> Articles { get; set; }
         public DbSet<Launch> Launches { get; set; }
