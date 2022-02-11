668cd5cf49e190ac0b2aad312734693b3a3b2f24
Karollainny Teles <karollainnyteles@hotmail.com>
2022-02-11 00:33:31 -0300

    Add repositories de Evnt e Launch

---
+++ src/Coodesh.Challenge.Data/Repositories/EventWriteOnlyRepository.cs
@@ -0,0 +1,40 @@
+﻿using Coodesh.Challenge.Business.Contracts.Repositories;
+using Coodesh.Challenge.Business.Models;
+using Coodesh.Challenge.Data.Context;
+using System;
+using System.Threading.Tasks;
+
+namespace Coodesh.Challenge.Data.Repositories
+{
+    public class EventWriteOnlyRepository : IEventWriteOnlyRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public EventWriteOnlyRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddAsync(Event entity)
+        {
+            await _dbContext.AddAsync(entity);
+        }
+
+        public Task RemoveAsync(Guid id)
+        {
+            _dbContext.Remove(new Event { Id = id });
+            return Task.CompletedTask;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public Task UpdateAsync(Event entity)
+        {
+            _dbContext.Update(entity);
+            return Task.CompletedTask;
+        }
+    }
+}
