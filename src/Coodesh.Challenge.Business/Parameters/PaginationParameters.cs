b075044926b9af4f5e9e92c58a7f689c2a0d48c2
Karollainny Teles <karollainnyteles@hotmail.com>
2022-02-11 00:40:05 -0300

    Modificando pagination

--- src/Coodesh.Challenge.Business/Parameters/PaginationParameters.cs
+++ src/Coodesh.Challenge.Business/Parameters/PaginationParameters.cs
@@ -5,6 +5,10 @@
         public int PageNumber { get; set; }
         public int PageSize { get; set; } = 10;
 
-        public int GetSkip() => PageSize * PageNumber;
+        public int GetSkip()
+        {
+            PageNumber = PageNumber < 1 ? 1 : PageNumber;
+            return (PageNumber - 1) * PageSize;
+        }
     }
 }
