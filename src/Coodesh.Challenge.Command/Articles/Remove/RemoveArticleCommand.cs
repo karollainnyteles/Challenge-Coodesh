c5acb26528d425dfabd889d9838326085343a79b
Karollainny Teles <karollainnyteles@hotmail.com>
2022-02-11 00:39:21 -0300

    add commands do remove

---
+++ src/Coodesh.Challenge.Command/Articles/Remove/RemoveArticleCommand.cs
@@ -0,0 +1,15 @@
+﻿using Coodesh.Challenge.Business.Models;
+using MediatR;
+
+namespace Coodesh.Challenge.Command.Articles.Remove
+{
+    public class RemoveArticleCommand : IRequest<Article>
+    {
+        public int Id { get; }
+
+        public RemoveArticleCommand(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ src/Coodesh.Challenge.Command/Articles/Remove/RemoveArticleCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Coodesh.Challenge.Business.Contracts.Repositories;
+using Coodesh.Challenge.Business.Models;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Coodesh.Challenge.Command.Articles.Remove
+{
+    public class RemoveArticleCommandHandler : IRequestHandler<RemoveArticleCommand, Article>
+    {
+        private readonly IArticleWriteOnlyRepository _articleWriteOnlyRepository;
+        private readonly IArticleReadOnlyRepository _articleReadOnlyRepository;
+
+        public RemoveArticleCommandHandler(IArticleWriteOnlyRepository articleWriteOnlyRepository,
+                                           IArticleReadOnlyRepository articleReadOnlyRepository)
+        {
+            _articleWriteOnlyRepository = articleWriteOnlyRepository;
+            _articleReadOnlyRepository = articleReadOnlyRepository;
+        }
+
+        public async Task<Article> Handle(RemoveArticleCommand command, CancellationToken cancellationToken)
+        {
+            var article = await _articleReadOnlyRepository.GetByIdAsync(command.Id);
+
+            if (article != null)
+            {
+                await _articleWriteOnlyRepository.RemoveAsync(article.Id);
+                await _articleWriteOnlyRepository.SaveChangesAsync();
+            }
+
+            return article;
+        }
+    }
+}
