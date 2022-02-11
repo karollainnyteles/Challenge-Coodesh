b8f42c8e2badcc9993823655477350ce20fa50e6
Karollainny Teles <karollainnyteles@hotmail.com>
2022-02-11 00:40:24 -0300

    modificando profile

--- src/Coodesh.Challenge.Command/Mappers/ArticleProfile.cs
+++ src/Coodesh.Challenge.Command/Mappers/ArticleProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Coodesh.Challenge.Business.Models;
 using Coodesh.Challenge.Command.Articles.Create;
+using Coodesh.Challenge.Command.Articles.Update;
 
 namespace Coodesh.Challenge.Command.Mappers
 {
@@ -9,10 +10,14 @@ namespace Coodesh.Challenge.Command.Mappers
         public ArticleProfile()
         {
             CreateMap<CreateArticleCommand, Article>();
+            CreateMap<UpdateArticleCommand, Article>();
 
             CreateMap<Article, CreateArticleResponse>().ReverseMap();
             CreateMap<Launch, CreateLaunchResponse>().ReverseMap();
             CreateMap<Event, CreateEventResponse>().ReverseMap();
+
+            CreateMap<UpdateEventCommand, Event>().ReverseMap();
+            CreateMap<UpdateLaunchCommand, Launch>().ReverseMap();
         }
     }
 }
