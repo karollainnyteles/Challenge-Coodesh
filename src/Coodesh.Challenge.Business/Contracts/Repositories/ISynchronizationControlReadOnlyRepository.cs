c0133e3480c65dc63c3cf4ef23403f67c5499c7c
Karollainny Teles <karollainnyteles@hotmail.com>
2022-02-11 21:52:11 -0300

    add interfaces de repositórios de SynchronizationControl

---
+++ src/Coodesh.Challenge.Business/Contracts/Repositories/ISynchronizationControlReadOnlyRepository.cs
@@ -0,0 +1,10 @@
+﻿using Coodesh.Challenge.Business.Models;
+using System.Threading.Tasks;
+
+namespace Coodesh.Challenge.Business.Contracts.Repositories
+{
+    public interface ISynchronizationControlReadOnlyRepository : IReadOnlyRepository<SynchronizationControl, int>
+    {
+        Task<SynchronizationControl> GetLast();
+    }
+}
