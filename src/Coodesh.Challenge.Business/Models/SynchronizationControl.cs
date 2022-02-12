a833db5745742f9f4d55a2bca8315db9b2cdb064
Karollainny Teles <karollainnyteles@hotmail.com>
2022-02-11 21:54:32 -0300

    Add entidade SynchronizationControl

---
+++ src/Coodesh.Challenge.Business/Models/SynchronizationControl.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Coodesh.Challenge.Business.Models
+{
+    public class SynchronizationControl : Entity<int>
+    {
+        public int ArticlesCount { get; set; }
+        public DateTime CreateAt { get; set; }
+    }
+}
